ca34e1e81811d5d8a71b24bb48ca169ffd329af9
MichaelHWatkins <michaelhugheswatkins@gmail.com>
2021-11-05 16:39:26 -0700

    display links WIP

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,25 @@
 //Business Logic
 function Receipt(){
-  this.order = [];
+  this.order = {};
+  this.orderNumber = 0;
   this.totalCost = 0;
 }
 
-Receipt.prototype.addPie = function(pie) {
-  this.order.push(pie);
+Receipt.prototype.addOrder = function(pie) {
+  pie.orderNumber = this.addOrderNumber();
+  this.order[pie.orderNumber] = pie;
   this.totalCost += pie.cost();
+}
+
+Receipt.prototype.addOrderNumber = function() {
+  this.orderNumber +=1;
+  return this.orderNumber;
 }
 
 function Pizza(){
   this.toppings = [];
   this.size = "";
+  this.name = "";
 }
 
 Pizza.prototype.cost = function() {
@@ -25,25 +33,43 @@ Pizza.prototype.cost = function() {
     return 15 + (this.toppings.length * 3);
   }
 }
+
+function attachContactListeners() {
+  
+}
 //UI Logic
+
 let newReceipt = new Receipt();
 $(document).ready(function() {
+
   $("#display-address-form").click(function(event) {
     event.preventDefault();
     $("#address-form").show();
   })
+ 
   $("form#new-pizza").submit(function(event) {
     event.preventDefault();
     const size = $("#size").val();
     const meat = $("#meat").val();
     const veg1 = $("#veg1").val();
     const veg2 = $("#veg2").val();
+    const name = $("#name").val();
     let newPizza = new Pizza();
+    newPizza.name = name;
     newPizza.size = size;
     newPizza.toppings.push(meat);
     newPizza.toppings.push(veg1);
     newPizza.toppings.push(veg2);
-    newReceipt.addPie(newPizza)
+    newReceipt.addOrder(newPizza)
+    $("ol#receipt").empty();
+    for (let i = 1; i <= newReceipt.orderNumber; i++) {
+      $("ol#receipt").append("<li id='" + newReceipt.order[i].orderNumber + "'>" + newReceipt.order[i].name + "<span id='pizza' class=" + newReceipt.order[i].orderNumber + 1 + ">"+ newReceipt.order[i].toppings[0] + ", " + 
+      newReceipt.order[i].toppings[1] + ", " + newReceipt.order[i].size + " = " + newReceipt.order[i].cost() + "</span></li>");
+      $("ol#receipt").on("click", "pizza", function() {
+        console.log("test")
+        $('#pizza').css({"display": "block"});
+      });
+    }
     $("#output").text("Cost: $" + newReceipt.totalCost );
   });
 });
